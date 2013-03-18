4932e11531eb92e1ddf52d2a02629e3892853b0c
Andrew Stewart <andrew.c.stewart@gmail.com>
2013-03-18 13:49:38 -0400

    Initial fab script.  Auth issues to work out for private repos.

---
+++ fabfile.py
@@ -0,0 +1,14 @@
+from fabric.api import *
+import fabtools
+from fabtools.vagrant import vagrant
+
+@task
+def deploy():
+    # Require SIdora from github
+    #fabtools.require.git.working_copy("https://github.com/Smithsonian/sidora.git")
+    run("git clone https://github.com/Smithsonian/sidora.git")
+
+@task
+def setup():
+    # Require git
+    fabtools.rpm.install('git')
